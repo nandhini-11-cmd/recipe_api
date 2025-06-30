94701cb0482d70e7d58395ecc8afcf68026745b8
nandhini-11-cmd <nandhini.ks11@gmail.com>
2025-06-30 13:01:51 +0530

    console update

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use('/recipe', recipeRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
